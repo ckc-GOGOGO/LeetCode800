5a28b1c3e2f2f05fb3f1830a94d3fade6b1db533
ckc-GOGOGO <80084404+ckc-GOGOGO@users.noreply.github.com>
2021-03-15 18:26:17 +0800

    Update Solution.java
    add 435 solution

--- Solution.java
+++ Solution.java
@@ -10,6 +10,26 @@ import java.util.concurrent.*;
 
 public class Solution {
     
+//    // leetcode 435
+//    public int eraseOverlapIntervals(int[][] intervals) {
+//        if (intervals.length == 0) return 0;
+//        Arrays.sort(intervals, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                return o1[1] - o2[1];
+//            }
+//        });
+//        int ans = 1;
+//        int right = intervals[0][1];
+//        for (int i = 1; i < intervals.length; i++) {
+//            if (intervals[i][0] >= right) {
+//                ans++;
+//                right = intervals[i][1];
+//            }
+//        }
+//        return intervals.length - ans;
+//    }
+    
 //     leetcode 1272
 //     public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
 //         ArrayList<List<Integer>> result = new ArrayList<>();
