3faf74c8ade0392fb30f3f5a3f70c590bda0adf3
ckc-GOGOGO <80084404+ckc-GOGOGO@users.noreply.github.com>
2021-03-16 20:48:35 +0800

    Update Solution.java
    add 137 solution

--- Solution.java
+++ Solution.java
@@ -10,6 +10,22 @@ import java.util.concurrent.*;
 
 public class Solution {
     
+//    leetcode 137
+//    public int singleNumber(int[] nums) {
+//        int[] count = new int[32];
+//        for (int num : nums) {
+//            for (int i = 0; i < 32; i++) {
+//                count[i] += num & 1;
+//                num = num >>> 1;
+//            }
+//        }
+//        int res = 0;
+//        for (int i = 0; i < 32; i++) {
+//            res = res | ((count[i] % 3) << i);
+//        }
+//        return res;
+//    }
+    
 //    // leetcode 435
 //    public int eraseOverlapIntervals(int[][] intervals) {
 //        if (intervals.length == 0) return 0;
